c7a9a74a591e549401e51c9203bd2554db261459
OscarOosterling <oscar-oosterling@live.nl>
2020-09-05 09:57:55 +0200

    Added binding and init and check methods, finished task 2

--- app/src/main/java/com/example/madlevel1task2/TruthTableActivity.kt
+++ app/src/main/java/com/example/madlevel1task2/TruthTableActivity.kt
@@ -2,10 +2,38 @@ package com.example.madlevel1task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.example.madlevel1task2.databinding.ActivityTruthtableBinding
 
 class TruthTableActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityTruthtableBinding
+    private var goodAnswers:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_truthtable)
+        binding = ActivityTruthtableBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+    private fun initViews(){
+        binding.btnSubmit.setOnClickListener(){
+            checkAnswers()
+        }
+    }
+    private fun checkAnswers(){
+        if(binding.edAnswer1.text.toString() == "T"){
+            goodAnswers+=1
+        }
+        if(binding.edAnswer2.text.toString() == "F"){
+            goodAnswers+=1
+        }
+        if(binding.edAnswer3.text.toString() == "F"){
+            goodAnswers+=1
+        }
+        if(binding.edAnswer4.text.toString() == "F"){
+            goodAnswers+=1
+        }
+        Toast.makeText(this, "Answers correct: $goodAnswers", Toast.LENGTH_SHORT).show()
+        goodAnswers = 0
     }
 }
